c925266d43002e5cb66534cf3bbb2927f17635a9
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-04-24 11:49:36 -0700

    comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # popup
-This popup only shows once per device and has both mobile and desktop sizes. It does use JavaScript.
+This popup only shows once per device. It is mobile first, with a desktop version, and does use JavaScript.  
+
+It is highly recommended you only edit fields in `custom.css` and `custom.js` when making changes to the ad styling and content. Please read ALL of the comments and keep a backup so you can revert any bad changes.
--- custom.js
+++ custom.js
@@ -1,3 +1,8 @@
+/**
+ * This is called by script.js "onload" to replace the text and images for
+ * the ad. If this is not present or a field is commented, the demo text
+ * and images will be shown. Only edit fields marked for editing.
+ */
 function loadtext() {
     var modal = document.getElementById("ca-popup-1");
     var title = modal.getElementsByTagName("h1")[0];
@@ -8,9 +13,24 @@ function loadtext() {
     var button = pictures[1];
     var logo = pictures[2];
 
+    /**
+     * EDIT
+     * These are the text for each part of the ad. You may also use HTML to style the text.
+     * DO NOT insert more elements (such as divs) into the text here. If you wish to remove 
+     * one of these fields, be sure to use "display: none;" in the appropriate media query 
+     * inside custom.css.
+     */
     title.innerHTML = "IT'S OUR WAY OF SAYING THANKS!";
     subtitle.innerHTML = "Cool savings on our magazine plus Digital Edition <b>FREE!</b>*";
     text.innerHTML = "*A one-year subscription will be added to your cart and you will be billed separately for the discounted price of $29.95. Offer only valid to those shipping within the United States. One offer per household.";
+    
+    /**
+     * EDIT
+     * These are the images as they appear in the ad. They should be stored in the Image 
+     * Manager on BigCommerce, the path of which is below:
+     * 
+     * //store-abc123.mybigcommerce.com/product_images/uploaded_images/example.png
+     */
     cover.src = "images/cover.png";
     button.src = "images/button.png";
     logo.src = "images/logo.png";
