e79c16bd411c18e64035615d5eb97b873bc42e36
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-13 12:49:26 -0700

    comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # popup
-This popup only shows once per device. It is mobile first, with a desktop version, and does use JavaScript.  
+This popup only shows once per device. It is designed mobile first and does use JavaScript. You may only edit fields in `custom.css` and `custom.js` when making changes to the ad styling and content. Please keep a backup so you can revert any bad changes. Please refer to the steps below, the manual, and the annotations in each document to understand how to change the ad. Thank you.
 
-It is highly recommended you only edit fields in `custom.css` and `custom.js` when making changes to the ad styling and content. Please read ALL of the comments and keep a backup so you can revert any bad changes.
+<!--TODO: Write the manual and the steps here-->
--- custom.js
+++ custom.js
@@ -1,37 +1,86 @@
 /**
- * This is called by script.js "onload" to replace the text and images for
- * the ad. If this is not present or a field is commented, the demo text
- * and images will be shown. Only edit fields marked for editing.
+ * PLEASE CAREFULLY READ THE README, THE MANUAL, AND ALL OF THE ANNOTATIONS IN
+ * THIS DOCUMENT TO UNDERSTAND HOW TO CHANGE...
+ * 
+ * THE TEXT, IMAGES, AND PROMOTION SKU
+ * 
+ * ...TO BE USED IN THE CHECKOUT AD ON BIGCOMMERCE. NOT FOLLOWING THEM WILL
+ * RESULT IN UNDESIRED EFFECTS.
+ * 
+ * YOU HAVE BEEN WARNED . . .
  */
+
+//-----------------------------------------------------------------------------
+// v v v v v v v v v v v (DO NOT EDIT BELOW THIS LINE) v v v v v v v v v v v v
+//-----------------------------------------------------------------------------
 function loadtext() {
-    var modal = document.getElementById("ca-popup-1");
-    var title = modal.getElementsByTagName("h1")[0];
-    var subtitle = modal.getElementsByTagName("h2")[0];
-    var text = modal.getElementsByTagName("p")[0];
-    var pictures = modal.getElementsByTagName("img");
-    var cover = pictures[0];
-    var button = pictures[1];
-    var logo = pictures[2];
+    var modal = document.getElementById("ca-popup-1")
+    var title = modal.getElementsByTagName("h1")[0]
+    var subtitle = modal.getElementsByTagName("h2")[0]
+    var text = modal.getElementsByTagName("p")[0]
+    var pictures = modal.getElementsByTagName("img")
+    var sub = document.getElementsByClassName("subscribe")[0]
+    var cover = pictures[0]
+    var button = pictures[1]
+    var logo = pictures[2]
+//-----------------------------------------------------------------------------
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ (DO NOT EDIT ABOVE THIS LINE) ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+//-----------------------------------------------------------------------------
 
     /**
-     * EDIT
-     * These are the text for each part of the ad. You may also use HTML to style the text.
-     * DO NOT insert more elements (such as divs) into the text here. If you wish to remove 
-     * one of these fields, be sure to use "display: none;" in the appropriate media query 
-     * inside custom.css.
+     * In this section you may ONLY EDIT THE DATA IN QUOTATION MARKS. DO NOT
+     * REMOVE FIELDS, RENAME THEM, OR LEAVE THEM BLANK. Each field is
+     * annotated, indicating what it corresponds to on the ad displayed at
+     * checkout. Please read EVERY ANNOTATION.
      */
-    title.innerHTML = "IT'S OUR WAY OF SAYING THANKS!";
-    subtitle.innerHTML = "Cool savings on our magazine plus Digital Edition <b>FREE!</b>*";
-    text.innerHTML = "*A one-year subscription will be added to your cart and you will be billed separately for the discounted price of $29.95. Offer only valid to those shipping within the United States. One offer per household.";
-    
+
     /**
-     * EDIT
-     * These are the images as they appear in the ad. They should be stored in the Image 
-     * Manager on BigCommerce, the path of which is below:
+     * These first three fields contain the text to be used in the ad. You may
+     * use standard HTML tags within the quotes to style the text. For example,
+     * to make some text bold:
      * 
-     * //store-abc123.mybigcommerce.com/product_images/uploaded_images/example.png
+     * "We're selling <b>GOOD STUFF</b>!"
      */
-    cover.src = "images/cover.png";
-    button.src = "images/button.png";
-    logo.src = "images/logo.png";
+
+    // This is the header text displayed at the top of the ad
+    title.innerHTML = "IT'S OUR WAY OF SAYING THANKS!"
+
+    // This is text displayed under the title of the ad
+    subtitle.innerHTML = "Cool savings on our magazine plus Digital Edition <b>FREE!</b>*"
+
+    // This is the fine print displayed at the bottom of the ad
+    text.innerHTML = "*A one-year subscription will be added to your cart and you will be billed separately for the discounted price of $29.95. Offer only valid to those shipping within the United States. One offer per household."
+
+    /**
+     * These next three fields are the pathway to the images that are displayed
+     * on the ad. Guidelines for the size of each image can be found in the
+     * manual. The pathway should ALWAYS have the structure and naming
+     * conventions below:
+     * 
+     * //s-storehash/product_images/uploaded_images/name.png
+     */
+
+    // This is the roughly half-width image displayed on one side of the ad
+    cover.src = "images/cover.png"
+
+    // This is the image for the subscribe button
+    button.src = "images/button.png"
+
+    // This is the image for the logo displayed below the subscribe button
+    logo.src = "images/logo.png"
+
+    // This is the SKU that will be added to the customers cart when they click
+    // the subscribe button
+    var sku = "MUS12CK"
+
+//-----------------------------------------------------------------------------
+// v v v v v v v v v v v (DO NOT EDIT BELOW THIS LINE) v v v v v v v v v v v v
+//-----------------------------------------------------------------------------
+    sub.onclick = function () {
+        hide()
+        // location.href = `/cart.php?action=add&sku=${sku}`
+    }
 }
+//-----------------------------------------------------------------------------
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ (DO NOT EDIT ABOVE THIS LINE) ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+//-----------------------------------------------------------------------------
